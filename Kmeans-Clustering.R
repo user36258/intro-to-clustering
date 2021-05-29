61a5f84b7f59af6efab13c9adf67e97d6404a5b6
user36258 <85011994+user36258@users.noreply.github.com>
2021-05-30 09:56:44 +1200

    change from mean shift to  kmeans add comments
    Original file was for k-means, re-written and added comments.

--- Kmeans-Clustering.R
+++ Kmeans-Clustering.R
@@ -1,42 +1,55 @@
 # k-means clustering example 
 
-# lets just make some test data. 
-
 library(tidyverse)
 library(cluster)
 library(factoextra)
 library(meanShiftR)
 
-zeroes = 3
+# size of test data, 3 = 1,000 rows, 2 = 100
+zeroes <- 3
+# k means requires you to tell it how many clusters there are 
+# the sample data will create 4 clusters 
+clusters <- 4
 
+# randomly select data from 1 - 6 with more 1's and 5's than other numbers 
 var_a <- sample(c(1,1,1,2,3,4,5,5,5,6), 1*10^zeroes, replace = TRUE)
 
+# do the same thing again 
 var_b <- sample(c(1,1,1,1,2,2,3,4,5,5,5,5,6), 1*10^zeroes, replace = TRUE)
 
+# create a dataframe of the sample data
 variables <- data.frame(var_a = var_a, 
                         var_b = var_b)
 
-### add some noise... 
-
+# add some noise to  the sample data 
+# add a random number from -1 to 1 to each variable 
 variables <- variables %>%
-  mutate(var_a = var_a+runif(1*10^zeroes, min = 0, max = 1),
-         var_b = var_b+runif(1*10^zeroes, min =0, max = 1))
+  mutate(var_a = var_a+runif(1*10^zeroes, min = -1, max = 1),
+         var_b = var_b+runif(1*10^zeroes, min = -1, max = 1))
 
+# plotting the pretend data to see how the clusters look 
 variables %>%
   ggplot(aes(x = var_a, y = var_b)) + 
   geom_point()
 
-variables <- as.matrix(variables)
+# running the k means function
+# one component of the result is a column with a number for each point 
+# with the cluster that the data point refers to 
 
-x  <- meanShiftR::meanShift(variables, variables)
+km  <- kmeans(variables, clusters)
 
-x
+# adding a column to the test data, with each cluster that the 
+# point falls in
+variables <- variables %>%
+  mutate(cluster = km$cluster)  %>%
+  # converting to factor, or ggplplot will shade it rather than 
+  # using discrete colours
+  mutate(cluster = factor(cluster, levels = 1:clusters))
 
-variables <- cbind(variables, x$assignment)
-
-variables %>%
-  ggplot(aes(x = var_a, y = var_b, col = assignment)) + 
-  geom_point()
+# plotting the test data with the colours indicating the cluster
+variables %>% 
+  ggplot(aes(x = var_a, y = var_b, col = cluster)) + 
+  geom_point() 
 
 
 
